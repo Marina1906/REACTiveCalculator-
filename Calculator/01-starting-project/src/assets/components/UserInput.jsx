a4ecca432cdef72e8774cc237e2f7f071e297e00
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-12-07 23:30:47 +0100

    Update UserInput.jsx

--- Calculator/01-starting-project/src/assets/components/UserInput.jsx
+++ Calculator/01-starting-project/src/assets/components/UserInput.jsx
@@ -1,27 +1,37 @@
+
+import { useState } from 'react';
+
 export default function UserInput () {
-    return <section id="user-input">
-<div className="input-group">
-    <p>
-        <label>Initial Investment</label>
-        <input type/>
-    </p>
-    <p>
-        <label >Annual Investment</label>
-        <input />
-    </p>
 
-</div>
-<div className="input-group">
-    <p>
-        <label>Expected Return</label>
-        <input type/>
-    </p>
-    <p>
-        <label >Duration</label>
-        <input />
-    </p>
+const [UserInput, setUserInput] =useState({
+    initialInvestment: 10000,
+    annuallInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
 
-</div>
-    </section>
+});
 
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>);
 }
