596658433c1d0f6b893efd0f8c3b427a3ee81cf2
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-12-08 00:14:48 +0100

    edited code

--- Calculator/01-starting-project/src/App.jsx
+++ Calculator/01-starting-project/src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./assets/components/Header.jsx"
+import UserInput from "./assets/components/UserInput.jsx"
 
 function App() {
   return (
+    <>
    <Header/>
-  )
+   <UserInput/>
+
+</>
+  );
 }
 
 export default App
--- Calculator/01-starting-project/src/assets/components/UserInput.jsx
+++ Calculator/01-starting-project/src/assets/components/UserInput.jsx
@@ -5,7 +5,7 @@ export default function UserInput () {
 
 const [UserInput, setUserInput] =useState({
     initialInvestment: 10000,
-    annuallInvestment: 1200,
+    annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10
 
@@ -34,17 +34,32 @@ function handleChange (inputIdentifier, newValue) {
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input type="number" required
+          value={UserInput.annualInvestment}
+          onChange={(event) =>
+          handleChange('annualInvestment', event.target.value)
+          } 
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input type="number" required
+          value={UserInput.expectedReturn}
+          onChange={(event) =>
+          handleChange('expectedReturn', event.target.value)
+          } 
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input type="number" required
+          value={UserInput.duration}
+          onChange={(event) =>
+          handleChange('duration', event.target.value)
+          } 
+          />
         </p>
       </div>
     </section>);
