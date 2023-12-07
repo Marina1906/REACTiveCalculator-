e54cae17870c94cd48a3209b7f663b3847035585
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-12-07 23:15:52 +0100

    edited code

---
+++ Calculator/01-starting-project/src/assets/components/UserInput.jsx
@@ -0,0 +1,27 @@
+export default function UserInput () {
+    return <section id="user-input">
+<div className="input-group">
+    <p>
+        <label>Initial Investment</label>
+        <input type/>
+    </p>
+    <p>
+        <label >Annual Investment</label>
+        <input />
+    </p>
+
+</div>
+<div className="input-group">
+    <p>
+        <label>Expected Return</label>
+        <input type/>
+    </p>
+    <p>
+        <label >Duration</label>
+        <input />
+    </p>
+
+</div>
+    </section>
+
+}
